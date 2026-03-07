63e8525b697cbe541a89bd10572e0a0fb70cd0cb
Valsecan <umutchapo8338@gmail.com>
2026-03-08 00:33:15 +0300

    Update index.js

--- index.js
+++ index.js
@@ -11,7 +11,6 @@ const client = new Client({
     GatewayIntentBits.GuildMembers,
     GatewayIntentBits.GuildBans,
     GatewayIntentBits.GuildModeration,
-    GatewayIntentBits.GuildRoles // 👈 Rol guard için gerekli
   ],
   partials: [Partials.Channel, Partials.GuildMember]
 });
