c2fb641b1c367c26fad951f6494f3be893010f7b
Valsecan <umutchapo8338@gmail.com>
2026-03-08 19:07:19 +0300

    Update index.js

--- index.js
+++ index.js
@@ -1,32 +1,26 @@
-const { Client, GatewayIntentBits, Collection } = require("discord.js")
-const fs = require("fs")
+const { Client, GatewayIntentBits } = require("discord.js")
 const express = require("express")
-const config = require("./config.json")
 
 const client = new Client({
  intents:[
   GatewayIntentBits.Guilds,
   GatewayIntentBits.GuildMembers,
-  GatewayIntentBits.GuildModeration,
-  GatewayIntentBits.GuildMessages
+  GatewayIntentBits.GuildModeration
  ]
 })
 
-client.commands = new Collection()
-
-// web server
+// web server (railway için)
 const app = express()
-app.get("/",(req,res)=>res.send("ATHENA GUARD aktif"))
-app.listen(8080,()=>console.log("Web server çalışıyor"))
+app.get("/",(req,res)=>res.send("Bot aktif"))
+app.listen(8080,()=>console.log("Web server 8080 portunda açık"))
 
-// sistemleri yükle
 require("./systems/panel")(client)
 require("./systems/guard")(client)
 require("./systems/backup")(client)
 require("./systems/whitelist")(client)
 
 client.once("ready",()=>{
- console.log(`Bot aktif: ${client.user.tag}`)
+ console.log(`Bot açıldı: ${client.user.tag}`)
 })
 
-client.login(config.token)
+client.login(process.env.TOKEN)
