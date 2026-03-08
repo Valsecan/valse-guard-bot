a17493334f2d44c1a8dd070b043dc215b79e7e2c
Valsecan <umutchapo8338@gmail.com>
2026-03-08 19:10:40 +0300

    Update backup.js

--- systems/backup.js
+++ systems/backup.js
@@ -5,11 +5,15 @@ module.exports = (client)=>{
 client.once("ready", async ()=>{
 
 const guild = client.guilds.cache.first()
+if(!guild) return
 
-const roles = guild.roles.cache.map(r=>({
-name:r.name,
-color:r.color,
-permissions:r.permissions.bitfield
+const roles = guild.roles.cache
+.filter(r => r.name !== "@everyone")
+.map(r => ({
+name: r.name,
+color: r.color,
+permissions: r.permissions.bitfield.toString(), // BIGINT FIX
+position: r.position
 }))
 
 fs.writeFileSync("./database/backup.json", JSON.stringify(roles,null,2))
@@ -18,4 +22,4 @@ console.log("Rol backup alındı")
 
 })
 
-}
+  }
